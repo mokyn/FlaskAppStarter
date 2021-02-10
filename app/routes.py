a956e77885b85077b140fcb57ec75d9b16960fac
Morris Kyn <morris.kyn@gmail.com>
2021-02-10 17:47:38 -0500

    Plant Identifier V1!

--- app/routes.py
+++ app/routes.py
@@ -9,7 +9,6 @@ import os
 from werkzeug.utils import secure_filename
 from werkzeug.datastructures import  FileStorage
 
-from app import piglatin
 from app import plantsearch
 
 try:
@@ -18,20 +17,50 @@ try:
 except:
     print("Make sure to pip install Pillow")
 
-# Home page, renders the index.html template
-@app.route('/index')
-@app.route('/')
-def index():
-    return render_template('index.html', title='Home')
+import base64
+import requests
+your_api_key = "GbOWe16RYiNsCsotfeAhO0l5zBx8bTEOdK74LXF8IrdHTywmxC"
 
-# Pig latin page, when we click translate, moves to text result page
-@app.route('/text',methods=['GET','POST'])
-def text():
+# Home page, renders the index.html template
+@app.route('/index',methods=['GET','POST'])
+@app.route('/',methods=['GET','POST'])
+def index():
     if request.method == 'POST':
-        old_text = request.form['text']
-        new_text = piglatin.pig_translate(old_text)
-        return render_template('textResults.html', old_text=old_text, new_text=new_text)
-    return render_template('text.html', title='Home')
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        # if the image is valid, do the following
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            # Create a path to the image in the upload folder, save the upload
+            # file to this path
+            save_old=(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            remember = filename
+            file.save(save_old)
+            with open(save_old, "rb") as file:
+                images = [base64.b64encode(file.read()).decode("ascii")]
+            json_data = {
+            "images": images,
+            "modifiers": ["similar_images"],
+            "plant_details": ["common_names", "url", "wiki_description", "taxonomy"]
+}
+            response = requests.post(
+                "https://api.plant.id/v2/identify",
+                json=json_data,
+                headers={
+                    "Content-Type": "application/json",
+                    "Api-Key": your_api_key
+                }).json()
+            rt = render_template('apiResults.html', data=response["suggestions"], filename=remember)
+            return rt
+    return render_template('index.html', title='Home')
 
 @app.route('/plantsearch',methods=['GET','POST'])
 def search():
@@ -50,38 +79,6 @@ def search():
 @app.route('/<filename>')
 def get_file(filename):
     return send_from_directory('static',filename)
-
-# Image uploading page, 
-@app.route('/image', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # If user does not select file, browser also
-        # submit an empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        # if the image is valid, do the following
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            # Create a path to the image in the upload folder, save the upload
-            # file to this path
-            save_old=(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            file.save(save_old)
-            # Take the image, make a new one that is inverted
-            img = Image.open(save_old)
-            rbg_img = img.convert('RGB')
-            inverted_image = PIL.ImageOps.invert(rbg_img)
-            save_new=(os.path.join(app.config['UPLOAD_FOLDER'], 'new_'+filename))
-            inverted_image.save(save_new)
-            # Render template with inverted picture
-            rt = render_template('imageResults.html', filename='new_'+filename)
-            return rt
-    return render_template('image.html')
 
 # allowed image types 
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
